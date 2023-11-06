030f671d2b136629afdef152ebb978c2330e3cf3
Devesh1741 <113981918+Devesh1741@users.noreply.github.com>
2023-11-06 17:13:01 +0000

    comprision

---
+++ 01_basic/04_comproision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1); // problem of conversion in two diff datatype
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // problem of conversion
+
+// console.log(undefined == 0 );
+// console.log(undefined > 0 );
+// console.log(undefined < 0 );
+
+// === strict check
+
+// console.log("2" === 2);
