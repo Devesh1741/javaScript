6b3aa4723362da01669d7a2da3c3088281abc68b
Devesh1741 <113981918+Devesh1741@users.noreply.github.com>
2023-11-06 17:05:07 +0000

    change is done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -26,3 +26,45 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 console.log(someNumber);
+
+
+
+
+
+//************************************** Operation *************************
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2-2);
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2**2); //4
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " devesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2"); //same as previous value
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32  this are depend on first come and first serve
+// console.log( (3 + 4) * 5 % 3); //this is the right technique
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num3, num4;
+
+num1 = num2 = num3 = 2 + 2 // Totally avoid this
+
+let gameCounter = 100 
+++gameCounter; //prefics
+gameCounter++; //postfics
+console.log(gameCounter);
