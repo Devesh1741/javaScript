eebe5c0c3cadc3bbee0b19fea17b2173cf6708c5
Devesh1741 <113981918+Devesh1741@users.noreply.github.com>
2023-11-06 16:46:36 +0000

    dataType_summry

---
+++ 01_basic/dataType_sammary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoogedIn = false
+const outsideTemp = null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 123333333333444444322n
+
+
+// Reference ( Non primitive)
+ 
+// Array, Object, Functions
+
+const heros = ("shaktiman", "naagraj", "doga");
+
+let myObj = {
+    name: "hitesh", 
+    age: 22
+}
+
+const myfunction = function(){
+console.log("Hello dev");
+
+}
+myfunction()
+
+console.log(typeof bigNumber);
+
