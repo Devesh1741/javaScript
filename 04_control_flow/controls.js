c66fe3170ece608722777c77eca073a7ed74ee2f
Devesh1741 <113981918+Devesh1741@users.noreply.github.com>
2024-02-02 11:16:14 +0000

    control_Flow_statement

---
+++ 04_control_flow/controls.js
@@ -0,0 +1,31 @@
+const balance = 1000
+
+if (balance === 1000) {
+    console.log("balanace is equale");
+}
+if (balance != 1000) {
+    console.log("balance is not equal to 1000");
+} else {
+    console.log("balance is equal to 1000");
+}
+
+switch (3) {
+    case 1:
+        console.log(january);
+        break;
+
+    case 2:
+        console.log(faburary);
+        break;
+    case 3:
+        console.log(march);
+        break;
+    case 4:
+        console.log(april);
+        break;
+
+    default:
+        console.log("this is default");
+        break;
+}
+
